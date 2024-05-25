b85684da84c666f32ee2568ea9f4674f3d2e60dc
Valentina Hernandez Bayona <valenh20023008@gmail.com>
2024-05-25 12:49:21 -0500

    Feat:URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,11 @@
 export const environment = {
   production: true,
   authURL: 'https://canelaauth-production.up.railway.app/api/talentsoft/auth',
+  //authURL: 'https://canelaauthh-qa.up.railway.app/api/talentsoft/auth',
   QaURL: 'https://talentsoftdespidos-qa.up.railway.app',
   CompanyURL: 'https://rootempresamicroservice-qa.up.railway.app',
   NominaURL: 'https://empresasnominamicroservice-qa.up.railway.app',
   SST: 'https://sstback-production.up.railway.app',
-  secretKey: 'ThisIsASecretKey'
+  RECLUTAMIENTO:'https://talentsoftreclutamientospring-production.up.railway.app',  secretKey: 'ThisIsASecretKey'
 };
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,10 @@
 export const environment = {
   production: false,
   authURL: 'https://canelaauth-qa.up.railway.app/api/talentsoft/auth',
+  //authURL: 'https://canelaauth-production.up.railway.app/api/talentsoft/auth',
   DespidosURL: 'https://talentsoftdespidos-qa.up.railway.app',
   CompanyURL: 'https://rootempresamicroservice-qa.up.railway.app',
   NominaURL: 'https://empresasnominamicroservice-qa.up.railway.app',
+  cardURL: 'NOSECUAL',
   secretKey: 'ThisIsASecretKey'
 };
